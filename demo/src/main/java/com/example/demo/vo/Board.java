3373ba0c763afcfa65dc88d8c81527b3b3268b89
user <user@DESKTOP-V882PTR>
2019-10-22 18:21:50 +0900

    .

---
+++ demo/src/main/java/com/example/demo/vo/Board.java
@@ -0,0 +1,85 @@
+package com.example.demo.vo;
+
+public class Board {
+	int id;
+	String userId;
+	String title;
+	String content;
+	
+	public Board() {}
+	public Board(int id, String userId, String title, String content) {
+		super();
+		this.id = id;
+		this.userId = userId;
+		this.title = title;
+		this.content = content;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + ((userId == null) ? 0 : userId.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Board other = (Board) obj;
+		if (content == null) {
+			if (other.content != null)
+				return false;
+		} else if (!content.equals(other.content))
+			return false;
+		if (id != other.id)
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (userId == null) {
+			if (other.userId != null)
+				return false;
+		} else if (!userId.equals(other.userId))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Board [id=" + id + ", userId=" + userId + ", title=" + title + ", content=" + content + "]";
+	}
+	
+	
+}
